54cd00f52818f9a5a9a96dbba936f92da505e3d9
manpreetsingh04m <ms2404mk@gmail.com>
2023-12-09 21:10:15 +0530

    Bug Fixed Completely

--- Counter/src/CounterClass.jsx
+++ Counter/src/CounterClass.jsx
@@ -1,47 +1,49 @@
-import { Component } from 'react'
+import  { Component } from 'react'
 import './App.css'
 
-class CounterApp extends Component{
-  constructor(props){
+class CounterApp extends Component {
+  constructor(props) {
     super(props)
-    this.state={
-      count:0
+    this.state = {
+      count: 0
     }
   }
-  increment=()=>{
+
+  increment = () => {
     this.setState({
-      count :this.state.count+1
+      count: this.state.count + 1
     })
   }
 
-  subtraction=()=>{
+  subtraction = () => {
+    if (this.state.count > 0) {
+      this.setState({
+        count: this.state.count - 1
+      })
+    }
+  }
+
+  reset = () => {
     this.setState({
-      count :this.state.count-1
+      count: 0
     })
   }
 
-  reset=()=>{
-    this.setState({
-      count :0
-    })
+  render() {
+    return (
+      <>
+        <div className='MainBox'>
+          <h1>COUNTER APP</h1>
+          <p className='count'>{this.state.count}</p>
+          <div className='buttons'>
+            <button className='increment' onClick={this.increment}>+</button>
+            <button className='subtraction' onClick={this.subtraction}>-</button>
+            <button className='reset' onClick={this.reset}>Reset</button>
+          </div>
+        </div>
+      </>
+    )
   }
-
-render(){
-  return(
-    <>
-    <div className='MainBox'>
-      <h1>COUNTER APP</h1>
-      <p className='count'>{this.state.count}</p>
-      <div className='buttons'>
-        <button className='increment' onClick={this.increment}>+</button>
-        <button className='subtraction' onClick={this.subtraction}>-</button>
-        <button className='reset' onClick={this.reset}>Reset</button>
-      </div>
-    </div>
-    </>
-  )
 }
-}
-
 
 export default CounterApp
